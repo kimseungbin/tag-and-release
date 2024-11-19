5e33cc60157efde538244738b7f3cae16f5ad4a6
SeungBin Kim <luxorienskim@gmail.com>
2024-11-19 22:09:04 +0900

    feat: Add label checker utility
    Introduces a `LabelChecker` class to automatically ensure predefined labels exist in the repository. This includes methods for checking existing labels and creating missing ones using the Octokit library.

---
+++ src/label-checker.ts
@@ -0,0 +1,59 @@
+import { Octokit } from '@octokit/rest'
+
+export class LabelChecker {
+	private readonly owner: string
+	private readonly repo: string
+	private octokit: Octokit
+	private labels = [
+		{
+			name: 'major',
+			description: 'Major version bump',
+			color: 'FF0000',
+		},
+		{
+			name: 'minor',
+			description: 'Minor version bump',
+			color: '00FF00',
+		},
+		{
+			name: 'patch',
+			description: 'Patch version bump',
+			color: '0000FF',
+		},
+	]
+
+	constructor(octokit: Octokit, owner: string, repo: string) {
+		this.owner = owner
+		this.repo = repo
+		this.octokit = octokit
+	}
+
+	async ensureLabelExist(): Promise<void> {
+		try {
+			const existingLabels = await this.octokit.rest.issues.listLabelsForRepo({
+				owner: this.owner,
+				repo: this.repo,
+			})
+
+			const existingLabelNames = existingLabels.data.map((label) => label.name)
+
+			await Promise.all(
+				this.labels
+					.filter((label) => !existingLabelNames.includes(label.name))
+					.map((label) => {
+						const { name, description, color } = label
+						return this.octokit.rest.issues.createLabel({
+							owner: this.owner,
+							repo: this.repo,
+							name,
+							description,
+							color,
+						})
+					}),
+			)
+		} catch (error) {
+			console.error(error)
+			throw new Error('Failed to check labels')
+		}
+	}
+}
