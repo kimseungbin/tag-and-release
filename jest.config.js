ffde268f7782f56c28968bd968dc549563d06ac7
kimseungbin <luxorienskim@gmail.com>
2024-11-19 14:47:20 +0900

    chore: improve jest config
    Add testMatch patterns to include more test files and set coverage thresholds for better test coverage enforcement. This helps ensure code quality and maintainability.

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
     transform: {
         '^.+.tsx?$': ['ts-jest', {}],
     },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 }
