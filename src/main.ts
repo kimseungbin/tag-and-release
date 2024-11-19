d4e50bd4d4f19d6eaf22330102ed77fc926fa877
kimseungbin <luxorienskim@gmail.com>
2024-11-19 14:38:55 +0900

    feat: add initial implementation of run function
    Introduce the main.ts file with a newly defined `run` function. This function fetches the 'name' input, logs a greeting message, and handles any potential errors.

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import {getInput, info, setFailed} from "@actions/core";
+
+export function run(): void {
+    try {
+        const name = getInput('name', {required: true})
+        info(`Hello ${name}!`)
+    } catch (error) {
+        setFailed((error as Error).message)
+    }
+}
