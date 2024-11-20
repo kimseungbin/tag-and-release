01775a8e8a80beab7153914c25ec2306bd0d94dc
SeungBin Kim <luxorienskim@gmail.com>
2024-11-20 22:54:32 +0900

    test: Refactor label-checker spec for Octokit instantiation
    Refactor the unit test setup to initialize Octokit separately before passing it to LabelChecker. This improvement promotes cleaner and more modular test code.

--- src/label-checker.spec.ts
+++ src/label-checker.spec.ts
@@ -2,10 +2,12 @@ import { LabelChecker } from './label-checker'
 import { Octokit } from '@octokit/rest'
 
 describe('Label Checker - GitHub Label Management', () => {
+	let octokit: Octokit
 	let labelChecker: LabelChecker
 
 	beforeEach(() => {
-		labelChecker = new LabelChecker(new Octokit(), 'owner', 'repo')
+		octokit = new Octokit()
+		labelChecker = new LabelChecker(octokit, 'owner', 'repo')
 	})
 
 	it('should create major, minor, and patch labels if they do not exist', async () => {
