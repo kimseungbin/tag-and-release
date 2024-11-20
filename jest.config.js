b8298b4f13efd024511d18e1a169d8c185e59115
kimseungbin <luxorienskim@gmail.com>
2024-11-20 14:11:17 +0900

    style: fix indentation in jest.config.js
    Corrected indentation in jest.config.js for improved readability. This change does

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-    testEnvironment: 'node',
-    transform: {
-        '^.+.tsx?$': ['ts-jest', {}],
-    },
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+	testEnvironment: 'node',
+	transform: {
+		'^.+.tsx?$': ['ts-jest', {}],
+	},
+	testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
 }
