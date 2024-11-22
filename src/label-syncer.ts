a6b9a504ac599a7573bde5dd918a46ac4a59d013
kimseungbin <luxorienskim@gmail.com>
2024-11-22 13:46:42 +0900

    feat: add basic label sync functionality
    Introduced the `syncLabels` method in the `LabelSyncer` class. Added a private method `selectHighestPriorityLabels` to select the highest priority label, though its implementation is currently pending.

--- src/label-syncer.ts
+++ src/label-syncer.ts
@@ -1,1 +1,10 @@
-export class LabelSyncer {}
+import { Label } from './label-config'
+
+export class LabelSyncer {
+	async syncLabels(): Promise<void> {}
+
+	private selectHighestPriorityLabels(labels: Label[]): Label {
+		// todo implement this
+		return labels[0]
+	}
+}
